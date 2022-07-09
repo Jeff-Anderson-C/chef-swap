fcc19c031b2f6dcbe947df64989d69d55b860a0b
Jeff-Anderson-C <80008336+Jeff-Anderson-C@users.noreply.github.com>
2022-07-08 19:12:38 -0500

    a

--- chef_swap/settings.py
+++ chef_swap/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['www.chefsourced.com', 'chefsourced.com']
+ALLOWED_HOSTS = ['www.chefsourced.com', 'chefsourced.com', 'coral-app-q5sh4.ondigitalocean.app']
 
 # os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
